a223aaf4c8649f2ce07423598240ccde0b880439
Abdul-Mateen <matdboi@gmail.com>
2022-07-12 02:45:01 +0100

    go

--- demo.go
+++ demo.go
@@ -13,6 +13,9 @@ func main() {
 	var link string
 	fmt.Print("Enter the link to process: ")
 	fmt.Scanf("%s", &link)
+	if link == "" {
+		link = "https://www.nairaland.com/7217386/strike-fg-breaks-asuus-rank"
+	}
 	page, err := http.Get(link)
 	logError(err)
 	pagetext, err := ioutil.ReadAll(page.Body)
@@ -29,7 +32,8 @@ func main() {
 	fmt.Println(doc.Data)
 	fmt.Println(doc.FirstChild)
 	fmt.Println(doc.NextSibling)*/
-	procNode(doc)
+	var cnt = 1
+	procNode(doc, cnt)
 
 }
 
@@ -39,12 +43,22 @@ func logError(err error) {
 	}
 }
 
-func procNode(node *html.Node) {
-	if node.Type == html.TextNode {
-		fmt.Println("-->", node.Data)
+func procNode(node *html.Node, cnt int) {
+	fmt.Println("Level", cnt)
+	if node.Type == html.ElementNode {
+		fmt.Println("E>", node.Data)
 	}
-	//fmt.Println(node.Type)
+	if node.Type == html.TextNode {
+		fmt.Println("T>", node.Data)
+	}
+	/*if node.Type == html.ElementNode && node.Data == "div" {
+		fmt.Println(node.FirstChild.Data)
+	}*/
+	//cnt = cnt + 1
 	for i := node.FirstChild; i != nil; i = i.NextSibling {
-		procNode(i)
+		procNode(i, cnt+1)
 	}
 }
+
+
+
